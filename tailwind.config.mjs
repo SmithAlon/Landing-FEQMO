a15c9f51c99cb854abd4d3a57c03281d3719660d
SmithAlon <smithalonso30@gmail.com>
2025-05-23 17:56:18 -0600

    LogoResponsive

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontWeight: {
+        medium: '500',
+      },
+    },
+  },
+  plugins: [],
+}
